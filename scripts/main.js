4f0c8160391c7010a797758b843a1cf6503b4fc7
mhdshs <83049900+mhdshs@users.noreply.github.com>
2021-04-23 06:55:36 +0430

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/pic-1.jpg') {
+      myImage.setAttribute('src','images/pic-2.jpg');
+    } else {
+      myImage.setAttribute('src','images/pic-1.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+myButton.onclick = function() {
+  setUserName();
+}
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
